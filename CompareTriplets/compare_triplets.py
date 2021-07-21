b1cd34afeca73149d606149eca65c5bb82111dc3
TRobibinson13 <trerrobinson@gmail.com>
2021-07-21 19:33:30 -0400

    Update compare_triplets.py
    Fixing my derpnesstivity

--- CompareTriplets/compare_triplets.py
+++ CompareTriplets/compare_triplets.py
@@ -1,21 +1,44 @@
-def compareTriplets(ar1, ar2):
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+a = [1,2,3]
+b = [3,2,1]
+
+def compareTriplets(a, b):
     arSizeMax = 3
     i = 0
-    ar1Points = 0
-    ar2Points = 0
+    aPoints = 0
+    bPoints = 0
+    arCompare = [0, 0]
 
-    validateTriplet(ar1)
-    validateTriplet(ar2)
+    validateTriplet(a)
+    validateTriplet(b)
 
     while i < arSizeMax :
-        if ar1[i] > ar2[i]:
-            ar1Points += 1
-        else:
-            
+        if a[i] > b[i]:
+            print(f'Triplet A has been awarded +1 point for the value at position {i}')
+            aPoints += 1
+        elif b[i] > a[i]:
+            print(f'Triplet B has been awarded +1 point for the value at {i}')
+            bPoints += 1
+        elif a[i] == b[i]:
+            print(f'No points awarded. Triplet values at position {i} are equal.')
         
+        i += 1
+
+    arCompare = [aPoints, bPoints]
+    print(f'The final scores.  Triplet A: {aPoints}.  Triplet B: {bPoints}')
+    return arCompare
+
 def validateTriplet(ar):
-    for x in triplet:
+    for x in ar:
         if x < 1 or x > 100:
             print(f'The following tripplet has invalid parameters: {ar}')
             return 0
 
+compareTriplets(a, b)
